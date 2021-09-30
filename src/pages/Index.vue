b0eb56b548d0cfc092123bc4bf1c0bb9e02b3c7c
Pfruit99 <64878953+Pfruit99@users.noreply.github.com>
2021-09-29 20:10:55 -0500

    Update Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -9,8 +9,8 @@
       <div class="container">
         <div class="content-center brand">
           <img class="n-logo" src="img/now-logo.png" alt="" />
-          <h1 class="h1-seo">Now UI Kit.</h1>
-          <h3>A beautiful Bootstrap 4 UI kit. Yours free.</h3>
+          <h1 class="h1-seo">Eventos Virtuales</h1>
+          <h3></h3>
         </div>
         <h6 class="category category-absolute">
           Designed by
